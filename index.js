8020769237055f308a67784794a9023823a38a16
matutter <mat@vm.home>
2016-09-28 23:00:30 -0400

    initial concept commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Promise = require('bluebird')
+const request = require('request')
+const Uri = require('./uri.js')
+
+const clients = require('./lib/clients')
+
+    //uri: 'http://localhost:5000/v2/_catalog/',
+    //uri: 'http://unix:/var/run/docker.sock:/images/json',
+
+var engine = new clients.DockerEngine({socket: '/var/run/docker.sock'})
+
+engine.images().then( images => {
+  console.log(' > ', images)
+})
+.catch(console.error.bind(console))

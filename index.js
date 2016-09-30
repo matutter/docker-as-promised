f2ec731c04768c081a840013bc3153b35c6f8b29
matutter <mat@vm.home>
2016-09-30 01:11:38 -0400

    working on DockerRegistryClient, man this is a shit interface

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Promise = require('bluebird')
 const request = require('request')
-const Uri = require('./uri.js')
 
 const clients = require('./lib/clients')
 
@@ -8,8 +7,37 @@ const clients = require('./lib/clients')
     //uri: 'http://unix:/var/run/docker.sock:/images/json',
 
 var engine = new clients.DockerEngine({socket: '/var/run/docker.sock'})
+var registry = new clients.DockerRegistry({host: 'localhost', port: 5000})
 
-engine.images().then( images => {
+/*engine.imagePush('localhost:5000/alpine').then( images => {
   console.log(' > ', images)
+})
+.catch(console.error.bind(console))*/
+
+
+
+registry.versionCheck().then( version => {
+  console.log(' > ', version)
+
+  return registry.catalog().then(catalog => {
+    console.log(catalog)
+    if(catalog.repositories) catalog.repositories.forEach(repo => {
+
+      return registry.delete(repo).then( manifest => {
+        console.log(`${manifest.name} deleted`)
+      })
+      /*registry.tags(repo).then(repo => {
+        console.log(repo)
+        if(repo.tags) repo.tags.forEach(tag => {
+          registry.manifests(repo.name, tag).then(manifest => {
+
+          }).catch(console.error.bind(console))
+        })
+      }).catch(console.error.bind(console))*/
+
+
+    })
+  })
+
 })
 .catch(console.error.bind(console))

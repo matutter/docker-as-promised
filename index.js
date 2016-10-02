cfd466917868c1b41ed43ae5d660f6e5c0bfaf33
matutter <matutter4@gmail.com>
2016-10-02 00:10:41 -0400

    added test packages, mocha, chai, coveralls, istanbul

--- index.js
+++ index.js
@@ -9,18 +9,3 @@ const clients = require('./lib/clients')
 var engine = new clients.DockerEngine({socket: '/var/run/docker.sock'})
 var registry = new clients.DockerRegistry({host: 'localhost', port: 5000})
 
-registry.versionCheck().then( version => 
-  registry.catalog().then(catalog => 
-    Promise.map(catalog.repositories, repo => 
-      registry.tags(repo).then(tags => Promise.map(tags, tag => 
-        registry.manifestRef(repo, tag).then( manifest => {
-          return registry.delete(repo, manifest.digest).then(() => {
-            return Promise.map(manifest.layers, layer => registry.deleteBlob(repo, layer.digest))
-          })
-        })
-      ))
-    )
-  )
-)
-.catch(e => console.log(e.data || e))
-

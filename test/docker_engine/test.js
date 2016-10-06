426b92fbd8f75edd2bb4ffe532a57ae2c72f4f84
matutter <matutter4@gmail.com>
2016-10-06 12:36:37 -0400

    removed unused artifacts

--- test/docker_engine/test.js
+++ test/docker_engine/test.js
@@ -5,8 +5,11 @@ const DockerEngine = require('../../lib/clients/DockerEngine.js')
 var engine = null
 
 describe(`DockerEngine`, () => {
+  /***
+  * initialization
+  * Test cases for initiating the engine client "new DockerEgnine(...)"
+  */
   describe('initialization', () => {
-
     it(`options { socket: 'unix socket' }`, () => {
       engine = new DockerEngine({ socket: '/var/run/docker.sock' })
     })
@@ -18,20 +21,42 @@ describe(`DockerEngine`, () => {
   })
 
   describe('utilities', () => {
+    /**
+    * utilities
+    * Utilities provided outside of the Docker v1.24 specification
+    * https://docs.docker.com/engine/reference/api/docker_remote_api_v1.24/
+    */
+    it('DockerEngine.ping checks the connection', () => {
+      engine.ping().should.be.fulfilled
+    })
 
-    it('DockerEngine.ping checks the connection', (done) => {
-      engine.ping().then(() => done()).catch(done)
+    /***
+    * All API in the /containers/* namespace
+    * specs: https://docs.docker.com/engine/reference/api/docker_remote_api_v1.24/#/3-1-containers
+    */
+    /**
+    * Ignores all properties that SHOULD not appear
+    */
+    it('should sanitize query string, removing properties not specified in specs', () => {
+      engine.containers({foo:'bar'}).should.be.fulfilled
+    })
+    /**
+    * Throws errors on properties that MAY appear
+    */
+    it(`should throw a TypeError with incorrect qs value types`, () => {
+      engine.containers({limit:'NaN'}).should.be.rejectedWith(TypeError)
+    })
+
+    it(`should make a request when the querystring parameters are of valid type`, () => {
+      return Promise.all([
+        engine.containers({limit: 1, all: '0', size: true, since : 'sha256:0123456789ABCDEF'}).should.be.fulfilled,
+        engine.ping().should.be.fulfilled
+      ])
     })
   })
 
   describe('3.1 containers', () => {
-    // specs: https://docs.docker.com/engine/reference/api/docker_remote_api_v1.24/#/3-1-containers
-    it('should sanitize query string as specified in specs', (done) => {
-      engine.containers({all:'fark'}).then(() => done()).catch(done)
-    })
-    it(`should validate all query string parameters specified in specs`, (done) => {
-      engine.containers({limit:1}).then(() => done()).catch(done)
-    })
+
   })
 
 })
--- test/test.js
+++ test/test.js
@@ -1,3 +1,14 @@
-assert = require('chai').assert
+chai = require('chai')
+chaiAsPromised = require('chai-as-promised')
 
+chai.should();
+chai.use(chaiAsPromised);
+
+global.chaiAsPromised = chaiAsPromised;
+global.expect = chai.expect;
+global.AssertionError = chai.AssertionError;
+global.Assertion = chai.Assertion;
+global.assert = chai.assert;
+
+//require('./docker_engine/validators.js')
 require('./docker_engine/test.js')

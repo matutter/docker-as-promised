f19cd60a6e70ddafe8f1a97b57076d53d55ee796
matutter <matutter4@gmail.com>
2016-10-24 23:51:23 -0400

    added more global test setup

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+fs = require('fs')
+path = require('path')
 chai = require('chai')
 chaiAsPromised = require('chai-as-promised')
 Promise = require('bluebird')
@@ -11,6 +13,17 @@ global.AssertionError = chai.AssertionError;
 global.Assertion = chai.Assertion;
 global.assert = chai.assert;
 
+global.__lib  = __lib = path.resolve('./lib')
+global.__schemas  = __schemas = __lib + '/schemas'
+
+/*console.log(fs.readdirSync(__lib))
+console.log(fs.readdirSync(__schemas))
+console.log(fs.readdirSync(__module))*/
+
+global.printError = printError = function(e) {
+  console.error(e)
+  throw e
+}
 
 global.wait = wait = function(ms) {
   return new Promise(resolve => {
@@ -18,9 +31,6 @@ global.wait = wait = function(ms) {
   })
 }
 
-//describe('JSONStream', () => {
-  //require('./json_stream/json_stream.js')
-//})
-
-//require('./docker_engine/validators.js')
+require('./misc.js')
+require('./schema/test.js')
 require('./docker_engine/test.js')
